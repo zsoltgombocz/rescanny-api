ce6b16d096edba8b1620810dbbb70f16705c6da5
Gömböcz Zsolt <gombocz.zsolt@fws.hu>
2026-01-02 21:34:20 +0100

    chore: Change the default timezone for users to Budapest

--- database/migrations/2025_11_02_100524_add_timezone_to_users.php
+++ database/migrations/2025_11_02_100524_add_timezone_to_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('timezone')->default('UTC');
+            $table->string('timezone')->default('Europe/Budapest');
         });
     }
 };
